13c49f8f6022931382754e1aee7c5f44758fd9b3
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-11-06 13:18:37 +0530

    Message auth using AES completed

--- middleware/messageAuth.js
+++ middleware/messageAuth.js
@@ -1,8 +1,19 @@
 const cryptojs = require("crypto-js");
+require("dotenv").config();
+
 function MessageAuthenticate(originalMessage, encryptedMessage) {
   //encrypt the original message
-  var encryptedTest = cryptojs.SHA256(originalMessage).toString();
+  var sendingTxt = cryptojs.enc.Utf8.parse(originalMessage);
+  var key = cryptojs.enc.Utf8.parse("JaNdRgUkXp2s5v8y");
+  var encrypted = cryptojs.AES.encrypt(sendingTxt, key, {
+    mode: cryptojs.mode.ECB,
+    padding: cryptojs.pad.ZeroPadding,
+  });
+  encrypted = encrypted.ciphertext.toString(cryptojs.enc.Hex);
+  var encryptedTest = encrypted;
 
+  console.log("Org enc ", encryptedMessage);
+  console.log("After enc ", encryptedTest);
   if (encryptedTest === encryptedMessage) {
     return true;
   } else {
